c596f6a6c1f6024066d9553f54ac4509ffeb5647
Patrick Casao <pcasao3@gmail.com>
2015-01-25 02:00:27 -0800

    Added GUI for timer and score, using old saucer

--- Abduction/Assets/GameController.cs
+++ Abduction/Assets/GameController.cs
@@ -6,21 +6,42 @@ public class GameController : MonoBehaviour {
 	//Object Variables
 	private static int points;
 	private static float timeLeft;
+	private static bool gameOver;
 
 	// Use this for initialization
 	void Start () {
 		points = 0;
 		timeLeft = 10.0f;
+		gameOver = false;
+	}
+
+	void OnGUI () {
+		if (gameOver) {
+			GUI.Label(new Rect(60,10,100,30), "GAME OVER");
+		}
+
+		else {
+    		GUI.Label(new Rect(60,10,100,30), "Timer: " + System.Math.Round(timeLeft, 2));
+		}
+
+		//Score GUI Label
+		GUI.Label(new Rect(60,40,100,30), "Score: " + points);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		timeLeft -= Time.deltaTime;
+
+
 		if (timeLeft <= 0){
 			Debug.Log("Game Over!!!");
-			//GameOver();
+			GameOver();
 		}
 	
+	}
+
+	public static void GameOver() {
+		gameOver = true;
 	}
 
 	public static void addPoints(int num) {
